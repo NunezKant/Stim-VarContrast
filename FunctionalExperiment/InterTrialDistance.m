bc393fae39af2d2ae7f14736b9b54d519369fb8c
NunezKant <35149535+NunezKant@users.noreply.github.com>
2021-12-20 12:04:09 -0500

    Add files via upload

--- FunctionalExperiment/InterTrialDistance.m
+++ FunctionalExperiment/InterTrialDistance.m
@@ -1,14 +1,15 @@
-function [] = InterTrialDistance(MoveSub,MoveMsgCount,distObjective)
+function [] = InterTrialDistance(win, texture_toshow, MoveSub, MoveMsgCount, distObjective)
     dist = 0; 
     while dist<distObjective
-    if MoveSub.MessageCount > MoveMsgCount
-        pause(2/1000); 
-        MoveMsgCount = MoveMsgCount + 1;
-        msg = MoveSub.read;
-        m = jsondecode(msg{1});
-    end   
-    combinedSpeed = (-m.pitch^2 + m.roll^2)^0.5;
-    dist = dist + combinedSpeed; 
+        if MoveSub.MessageCount > MoveMsgCount
+            pause(2/1000); 
+            MoveMsgCount = MoveMsgCount + 1;
+            msg = MoveSub.read;
+            m = jsondecode(msg{1});
+        end   
+        combSpeed = (-m.pitch^2 + m.roll^2)^0.5;
+        dist = dist + combSpeed; 
+        Screen('DrawTexture', win, texture_toshow, [], [], [], 1, 0);
     end    
 end
 
