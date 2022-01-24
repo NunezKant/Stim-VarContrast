72335bc875520d50402ea3b85b7de6b106eea5a8
NunezKant <35149535+NunezKant@users.noreply.github.com>
2022-01-24 13:48:27 -0500

    Add files via upload

--- FunctionalExperiment/ResizeNGreyCentering.m
+++ FunctionalExperiment/ResizeNGreyCentering.m
@@ -1,10 +1,16 @@
-function [RwdImg,NoRwdImg] = ResizeNGreyCentering(RwdImg,NoRwdImg,grey,screenNumber)
+function [RwdImg,NoRwdImg] = ResizeNGreyCentering(RwdImg,NoRwdImg,grey,screenNumber, surrounding)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
     RwdImg = RwdImg + (grey-mean2(RwdImg)) - grey;
     NoRwdImg = NoRwdImg +(grey-mean2(NoRwdImg)) - grey;
-    [width, height]=Screen('WindowSize', screenNumber);
-    RwdImg = imresize(RwdImg, [height width]);
-    NoRwdImg = imresize(NoRwdImg, [height width]);
+    
+    if surrounding
+        RwdImg = imresize(RwdImg, [1540 2048*3]);
+        NoRwdImg = imresize(NoRwdImg, [1540 2048*3]);
+    else
+        [width, height]=Screen('WindowSize', screenNumber);
+        RwdImg = imresize(RwdImg, [height width]);
+        NoRwdImg = imresize(NoRwdImg, [height width]);
+    end
 end
 
