6f0ba081ca58cdf9e12a3c2714933cea6c14a5fa
NunezKant <35149535+NunezKant@users.noreply.github.com>
2022-01-31 12:31:08 -0500

    Delete FunctionalExperiment directory

--- FunctionalExperiment/DeliverReward.m
+++
@@ -1,5 +0,0 @@
-function [RewardCount] = DeliverReward(myMQTT,RewardCount,ms)
-    publish(myMQTT, 'miniBCS/Valve1/Pulse', num2str(ms));
-    RewardCount = RewardCount + 1;
-end
-
--- FunctionalExperiment/ReadLick.m
+++
@@ -1,9 +0,0 @@
-function [Licks,LickMsgCount, IsLicking] = ReadLick(Licks,LickSub,LickMsgCount,trial,distance,alpha,Isrewarded)
-    IsLicking = 0;
-    if LickSub.MessageCount > LickMsgCount
-        LickMsgCount = LickMsgCount + 1;
-        IsLicking = 1;
-        Licks(end+1,:) = [trial distance alpha Isrewarded now];
-    end
-end
-
