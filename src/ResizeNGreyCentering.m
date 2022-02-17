252986254b3eec5f2ed2ef5a2861180fb18f44c1
NunezKant <35149535+NunezKant@users.noreply.github.com>
2022-02-17 15:38:12 -0500

    Add files via upload

--- src/ResizeNGreyCentering.m
+++ src/ResizeNGreyCentering.m
@@ -1,14 +1,16 @@
-function [RwdImg,NoRwdImg] = ResizeNGreyCentering(RwdImg,NoRwdImg,grey,screenNumber,surrounding,res)
+function [RwdImg,NoRwdImg,New_rwrd_img] = ResizeNGreyCentering(RwdImg,NoRwdImg,New_rwrd_img,grey,screenNumber,surrounding,res)
     RwdImg = RwdImg + (grey-mean2(RwdImg)) - grey;
     NoRwdImg = NoRwdImg +(grey-mean2(NoRwdImg)) - grey;
-    
+    New_rwrd_img = New_rwrd_img + (grey-mean2(New_rwrd_img)) - grey;
     if surrounding
         RwdImg = imresize(RwdImg, [res(2) res(1)]);
         NoRwdImg = imresize(NoRwdImg, [res(2) res(1)]);
+        New_rwrd_img = imresize(New_rwrd_img, [res(2) res(1)]);
     else
         [width, height]=Screen('WindowSize', screenNumber);
         RwdImg = imresize(RwdImg, [height width]);
         NoRwdImg = imresize(NoRwdImg, [height width]);
+        New_rwrd_img = imresize(New_rwrd_img, [height width]);
     end
 end
 
--- src/UploadImages.m
+++ src/UploadImages.m
@@ -1,20 +1,24 @@
-function [rwrd_img,Norwd_img] = UploadImages()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-[file_r,path_r] = uigetfile({'*';'*.png';'*.jpeg'},'Select the rewarded texture');
-[file_nr,path_nr] = uigetfile({'*';'*.png';'*.jpeg'},'Select the NON rewarded texture');
-rwrd_img = imread(strcat(path_r,file_r));
-Norwd_img = imread(strcat(path_nr,file_nr));
-if length(size(rwrd_img))>2
-    rwrd_img = im2double(rgb2gray(rwrd_img));
-else
-    rwrd_img = im2double(rwrd_img);
-end
-if length(size(Norwd_img))>2
-    Norwd_img = im2double(rgb2gray(Norwd_img));
-else
-    Norwd_img = im2double(Norwd_img);
-end
+function [rwrd_img,Norwd_img,New_rwrd_img] = UploadImages()
+    % [file_r,path_r] = uigetfile('../data/',{'*';'*.png';'*.jpeg'},'Select the rewarded texture');
+    % [file_nr,path_nr] = uigetfile('../data/',{'*';'*.png';'*.jpeg'},'Select the NON rewarded texture');
+    rwrd_img = imread('../data/leaves.jpg');
+    Norwd_img = imread('../data/circles.jpg');
+    New_rwrd_img = imread('../data/newleaves.jpg');
 
+    if length(size(rwrd_img))>2
+        rwrd_img = im2double(rgb2gray(rwrd_img));
+    else
+        rwrd_img = im2double(rwrd_img);
+    end
+    if length(size(Norwd_img))>2
+        Norwd_img = im2double(rgb2gray(Norwd_img));
+    else
+        Norwd_img = im2double(Norwd_img);
+    end
+    if length(size(New_rwrd_img))>2
+        New_rwrd_img = im2double(rgb2gray(New_rwrd_img));
+    else
+        New_rwrd_img = im2double(New_rwrd_img);
+    end
 end
 
