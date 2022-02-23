799add32826e3babd9d55d2978589fdeff726b91
NunezKant <35149535+NunezKant@users.noreply.github.com>
2022-02-23 15:00:22 -0500

    Add files via upload

--- src/RandomImgSelection.m
+++ src/RandomImgSelection.m
@@ -1,8 +1,8 @@
 function [texture_toshow] = RandomImgSelection(imagtex_reward,imagtex_non_reward,imagtex_new_reward,rewarded,new_exemplar)
-    if rewarded && ~new_exemplar
-        texture_toshow=imagtex_reward;
-    elseif rewarded && new_exemplar
+    if rewarded && new_exemplar
         texture_toshow=imagtex_new_reward;
+    elseif rewarded 
+        texture_toshow=imagtex_reward;
     else 
         texture_toshow=imagtex_non_reward;
     end
