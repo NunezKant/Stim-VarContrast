581f9b22bc08f4c99d5f9bbc21089687971cc61f
NunezKant <35149535+NunezKant@users.noreply.github.com>
2023-05-06 11:25:31 -0400

    check grey correction and added gray option

--- src/UploadImages.m
+++ src/UploadImages.m
@@ -3,40 +3,40 @@ function [rwrd_img,Norwd_img,New_rwrd_img,New_nrwrd_img] = UploadImages(texturec
     % [file_nr,path_nr] = uigetfile('../data/',{'*';'*.png';'*.jpeg'},'Select the NON rewarded texture');
     switch texturecategory
         case "leavescircles"
-        rwrd_img = imread('./data/leaves.jpg');
-        Norwd_img = imread('./data/circles.jpg');
-        New_rwrd_img = imread('./data/newleaves.jpg');
-        New_nrwrd_img = imread('./data/newcircles.jpg');
+        rwrd_img = im2double(imread('./data/leaves.jpg'));
+        Norwd_img = im2double(imread('./data/circles.jpg'));
+        New_rwrd_img = im2double(imread('./data/newleaves.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newcircles.jpg'));
         case "circlesleaves"
-        rwrd_img = imread('./data/circles.jpg');
-        Norwd_img = imread('./data/leaves.jpg');
-        New_rwrd_img = imread('./data/newcircles.jpg');
-        New_nrwrd_img = imread('./data/newleaves.jpg');
+        rwrd_img = im2double(imread('./data/circles.jpg'));
+        Norwd_img = im2double(imread('./data/leaves.jpg'));
+        New_rwrd_img = im2double(imread('./data/newcircles.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newleaves.jpg'));
         case "tilessquares"
-        rwrd_img = imread('./data/mandala.jpg');
-        Norwd_img = imread('./data/squares.jpg');
-        New_rwrd_img = imread('./data/newmandala.jpg');
-        New_nrwrd_img = imread('./data/newsquares.jpg');
+        rwrd_img = im2double(imread('./data/mandala.jpg'));
+        Norwd_img = im2double(imread('./data/squares.jpg'));
+        New_rwrd_img = im2double(imread('./data/newmandala.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newsquares.jpg'));
         case "squarestiles"
-        rwrd_img = imread('./data/squares.jpg');
-        Norwd_img = imread('./data/mandala.jpg');
-        New_rwrd_img = imread('./data/newsquares.jpg');
-        New_nrwrd_img = imread('./data/newmandala.jpg');
+        rwrd_img = im2double(imread('./data/squares.jpg'));
+        Norwd_img = im2double(imread('./data/mandala.jpg'));
+        New_rwrd_img = im2double(imread('./data/newsquares.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newmandala.jpg'));
         case "leavestiles"
-        rwrd_img = imread('./data/leaves.jpg');
-        Norwd_img = imread('./data/mandala.jpg');
-        New_rwrd_img = imread('./data/newleaves.jpg');
-        New_nrwrd_img = imread('./data/newmandala.jpg');
+        rwrd_img = im2double(imread('./data/leaves.jpg'));
+        Norwd_img = im2double(imread('./data/mandala.jpg'));
+        New_rwrd_img = im2double(imread('./data/newleaves.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newmandala.jpg'));
         case "tilesleaves"
-        rwrd_img = imread('./data/mandala.jpg');
-        Norwd_img = imread('./data/leaves.jpg');
-        New_rwrd_img = imread('./data/newmandala.jpg');
-        New_nrwrd_img = imread('./data/newleaves.jpg');
+        rwrd_img = im2double(imread('./data/mandala.jpg'));
+        Norwd_img = im2double(imread('./data/leaves.jpg'));
+        New_rwrd_img = im2double(imread('./data/newmandala.jpg'));
+        New_nrwrd_img = im2double(imread('./data/newleaves.jpg'));
         case "gray"
-        rwrd_img = imread('./data/gray.jpg');
-        Norwd_img = imread('./data/gray.jpg');
-        New_rwrd_img = imread('./data/gray.jpg');
-        New_nrwrd_img = imread('./data/gray.jpg');
+        rwrd_img = im2double(imread('./data/gray.jpg'));
+        Norwd_img = im2double(imread('./data/gray.jpg'));
+        New_rwrd_img = im2double(imread('./data/gray.jpg'));
+        New_nrwrd_img = im2double(imread('./data/gray.jpg'));
     end
 
     if length(size(rwrd_img))>2
